b489c0a43678f436709a11bef47ded2f52bea1a4
github username <github email address>
2021-04-12 10:08:55 -0400

    added expereince

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
+import Experience from './components/Experience';
 
 function App() {
 	return (
@@ -12,10 +13,14 @@ function App() {
 
 				<div className="px-12 py-12 box-content">
 					<Switch>
-						<Route exact path="/" component={Home} />
 						<Route path="/projects" component={Projects} />
 						<Route path="/about" component={About} />
-						{/* <Route path="/about" component={About} /> */}
+						<Route path="/experience">
+							<Experience />
+						</Route>
+						<Route exact path="/">
+							<Home />
+						</Route>
 					</Switch>
 				</div>
 			</Router>
---
+++ src/components/data.js
@@ -0,0 +1,40 @@
+const data = [
+	{
+		id: 'job1',
+		title: 'Web Developer',
+		order: 3,
+		dates: 'October 2020 - Present',
+		duties: [
+			'Create programs for my teammates to get work done faster with code automation in Excel using VBA to convert cells with data into HTML output for our websites.',
+			'Writing serveless functions in AWS Lambda convert audio files in one S3 bucket to text using AWS Transcribe then sending the text file output to be stored in another S3 bucket.',
+			'Building small apps for uploading and storing files in s3 with python and Django',
+		],
+		company: 'Amazon',
+	},
+	{
+		id: 'job2',
+		title: 'Web Producer',
+		order: 2,
+		dates: 'April 2019 - October 2020',
+		duties: [
+			'Build websites using pure html and css',
+			'Write documentation on new technologies that were presented to the team by me.',
+			'Manage tasks in Jira.',
+		],
+		company: '2U',
+	},
+	{
+		id: 'job3',
+		title: 'Web Developer / Part-time',
+		order: 3,
+		dates: 'Auguts 2019 - Present',
+		duties: [
+			'Setting DNS and hosting for my client and configuring mail servers.',
+			'Creating website pages within Wordpress',
+			'Manage tasks in Jira.',
+		],
+		company: 'Rockhouse',
+	},
+];
+
+export default data;
